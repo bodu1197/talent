dc3ed863b7c608b58f526bfa120f1fbdd140ff21
Your Name <your_email@example.com>
2025-12-13 05:56:58 +0700

    test: Temporarily remove HeroSection for diagnostics
    Temporarily commented out the HeroSection component from the homepage to isolate and diagnose the cause of the high LCP. This will help determine if the HeroSection is the primary bottleneck.

--- src/components/common/HeroWithCategories.tsx
+++ src/components/common/HeroWithCategories.tsx
@@ -1,13 +1,13 @@
-import HeroSection from '@/components/home/HeroSection'
-import CategoryGrid from '@/components/home/CategoryGrid'
-import MobileSearchBar from '@/components/home/MobileSearchBar'
+// import HeroSection from '@/components/home/HeroSection'
+import CategoryGrid from '@/components/home/CategoryGrid';
+import MobileSearchBar from '@/components/home/MobileSearchBar';
 
 export default function HeroWithCategories() {
   return (
     <>
-      <HeroSection />
+      {/* <HeroSection /> */}
       <MobileSearchBar />
       <CategoryGrid />
     </>
-  )
+  );
 }
